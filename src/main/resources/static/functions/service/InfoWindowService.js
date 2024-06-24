e7342fa05407550a6b1b21b4477e823cfb7845ff
Danil <usadanilakl@gmail.com>
2024-06-24 12:28:10 -0500

    info window works, fixed resizers on it

--- src/main/resources/static/functions/service/InfoWindowService.js
+++ src/main/resources/static/functions/service/InfoWindowService.js
@@ -11,7 +11,10 @@ initResize(infoFrame,false);
 let grab = infoFrame.querySelector('#grab')
 let p = document.createElement('p');
 p.textContent = "Info";
-grab.classList.add('move');
+grab.setAttribute('class','move');
 grab.style.top = '0px';
 grab.setAttribute('id','infoMover')
 grab.appendChild(p);
+infoFrame.querySelectorAll('.corners').forEach(e=>{
+    e.setAttribute('class', 'infoWindowCorners')
+})
