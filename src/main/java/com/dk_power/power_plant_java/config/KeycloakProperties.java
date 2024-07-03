4efc649f36b89622d3e2af8f2dcd8e93bc399f9e
Danil <usadanilakl@gmail.com>
2024-07-03 04:28:52 -0500

    Working on excel point info window. Form is alredy built.

--- src/main/java/com/dk_power/power_plant_java/config/KeycloakProperties.java
+++ src/main/java/com/dk_power/power_plant_java/config/KeycloakProperties.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Getter
 @Setter
 public class KeycloakProperties {
--- src/main/resources/static/functions/service/PointService.js
+++ src/main/resources/static/functions/service/PointService.js
@@ -15,3 +15,42 @@ function formatLabel(label){
     // console.log("result: "+result)
     return result;
 }
+
+function showPointInfo(point){
+    let form = document.createElement('form');
+    for(let e in point){
+        let div = document.createElement('div');
+        form.appendChild(div);
+        div.classList.add('form-group');
+        let label = document.createElement('label');
+        div.appendChild(label);
+        label.setAttribute('for',e);
+        let input = document.createElement('input');
+        div.appendChild(input);
+        input.setAttribute('id',e);
+        input.value = point[e];
+        input.readOnly = true;
+    }
+    return form;
+}
+
+function excelPointDropdown(points){
+    let list = document.createElement('ul');
+    points.forEach(e=>{
+        let item = document.createElement('li');
+        list.appendChild(item);
+        let formContainer = document.createElement('div');
+        item.appendChild(div);
+        let button = document.createElement('button');
+        item.appendChild(button);
+        button.textContent = e.label;
+        button.addEventListener('click', ()=>{
+            if(formContainer.children.length === 0){
+               div.appendChild(showPointInfo(e));
+            }else{
+                formContainer.innerHTML = "";
+            }
+        })
+    })
+    return list;
+}
