2810a4a43948e813044ae8b804d97fe45d17f620
usadanilakl <usadanilakl@gmail.com>
2024-07-04 14:26:44 -0500

    point table sort/search works. Need to work on performance efficiency - load partually

--- src/main/resources/static/functions/service/TableService.js
+++ src/main/resources/static/functions/service/TableService.js
@@ -9,6 +9,7 @@ let lastSortedBy = "";
 
 function createTableFromObjects(objects){
     filteredArray = objects;
+    console.log(filteredArray.length);
     let table = document.createElement('table');
     table.classList.add('table');
     table.classList.add('table-dark');
@@ -18,6 +19,10 @@ function createTableFromObjects(objects){
     let header = document.createElement('tr');
     thead.appendChild(header);
 
+    let index = document.createElement('th');
+    header.appendChild(index);
+    index.textContent = 'Index';
+
     for(let e in objects[0]){
         let th = document.createElement('th');
         header.appendChild(th);
@@ -25,22 +30,32 @@ function createTableFromObjects(objects){
         th.appendChild(search);
         search.setAttribute('type','text');
         search.setAttribute('placeholder','filter');
-        filteredArray = filterObjects(e,search.value,objects);
-        search.addEventListener('change', ()=>createTableFromObjects(filteredArray));
+        search.addEventListener('change', ()=>{
+            filteredArray = filterObjects(e,search.value,objects);
+            tableContainer.innerHTML = "";
+            tableContainer.appendChild(createTableFromObjects(filteredArray));
+    });
         let button = document.createElement('button');
         th.appendChild(button);
         button.textContent = e;
         button.addEventListener('click',()=>{
             sortObjects(e);
-            createTableFromObjects(filteredArray);
+            tableContainer.innerHTML = "";
+            tableContainer.appendChild(createTableFromObjects(filteredArray));
         });
     }
 
     let tbody = document.createElement('tbody');
     table.appendChild(tbody);
+    let i = 1;
     filteredArray.forEach(el=>{
         let row = document.createElement('tr');
         tbody.appendChild(row);
+
+        let indexData = document.createElement('td');
+        indexData.textContent = i++;
+        row.appendChild(indexData);
+
         for(let key in el){
             let td = document.createElement('td');
             td.textContent = el[key];
@@ -52,19 +67,22 @@ function createTableFromObjects(objects){
 
 function filterObjects(key,value, objects){
     return objects.filter(e=>{
-        e[key].trim().toLowerCase().includes(value.trim().toLowerCase());
+        //console.log(e[key] + ", " + value);
+        return String(e[key]).trim().toLowerCase().includes(value.trim().toLowerCase());
     })
 }
 
 function sortObjects(key){
+    console.log(lastSortedBy);
     if(lastSortedBy !== key){
+        console.log(key)
             filteredArray.sort((a,b)=>{
-            return a[key] - b[key];
+                return a[key].toString().localeCompare(b[key].toString());
         });
         lastSortedBy = key;
     }else{
         filteredArray.sort((a,b)=>{
-            return b[key] - a[key];
+            return b[key].toString().localeCompare(a[key].toString());
         });
         lastSortedBy = "";
     }
