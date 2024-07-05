681e644642bda9ff448d675771a134abb938b09b
Danil <usadanilakl@gmail.com>
2024-07-05 15:09:47 -0500

    Working on adding points to loto.

--- src/main/resources/static/functions/service/LotoService.js
+++ src/main/resources/static/functions/service/LotoService.js
@@ -1,4 +1,5 @@
 let lotoWindow = null;
+let lotoPoints = [];
 
 function lotoModeControl(){
     if(modes.lotoMode.state){
@@ -24,10 +25,9 @@ function setUpLotoWindow(){
 function addPointToLotoWindow(point){
     if(modes.lotoMode.state){
         let tbody = lotoWindow.querySelector('tbody');
-        let index = tbody.rows.length;
+        let index = tbody.rows.length+1;
         tbody.appendChild(createRow(index,point)); 
     }
-    
 }
 
 function createTable(object){
@@ -64,11 +64,66 @@ function createRow(rowNum, obj){
     indexData.textContent = rowNum;
     row.appendChild(indexData);
     indexData.classList.add('idexData');
+    let label = obj.label;
 
     for(let key in object){
         let td = document.createElement('td');
         td.textContent = object[key];
         row.appendChild(td);
     }
+
+    row.appendChild(createRemoveButton(label))
+    addObjectToArr(lotoPoints,obj);
     return row;
+}
+
+function addObjectToArr(arr,obj){
+    if(!arr.find(e=>e.label===obj.label)) arr.push(obj);
+}
+
+function createRemoveButtons(){
+    if(lotoWindow!==null){
+        let tbody = lotoWindow.querySelector('.tbody');
+        tbody.rows.forEach(r=>{
+        let td = document.createElement('td');
+        r.appendChild(td);
+        let button = document.createElement('button');
+        td.appendChild(button);
+        button.textContent("Remove");
+        let label = r.cells[1].value;
+        button.addEventListener('click',()=>{
+            lotoPoints.filter(e=>!e.label.includes(label));
+            tbody.innerHTML = "";
+            lotoPoints.forEach(e=>addPointToLotoWindow(e))
+        });
+        
+    });
+    }
+    
+}
+
+function createRemoveButton(label){
+    let tbody = lotoWindow.querySelector('tbody');
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    td.appendChild(button);
+    button.textContent = "Remove";
+    button.addEventListener('click',()=>{
+        for (let i = lotoPoints.length - 1; i >= 0; i--) {
+            if (lotoPoints[i].label.includes(label)) {
+                lotoPoints.splice(i, 1);
+            }
+        }
+        tbody.innerHTML = "";
+        lotoPoints.forEach(e=>addPointToLotoWindow(e))
+    });
+    
+    return td;
+    
+    
+}
+
+function submitLotoPoints(loto){
+    //update loto on server (temp loto)
+    //redirect to loto create page
 }
