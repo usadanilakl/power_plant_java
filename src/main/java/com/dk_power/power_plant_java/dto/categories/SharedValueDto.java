5960e782518a44361e4017775602cd883a79d528
Danil <usadanilakl@gmail.com>
2024-07-22 18:48:26 -0500

    Need to test point crud functions

---
+++ src/main/java/com/dk_power/power_plant_java/dto/categories/SharedValueDto.java
@@ -0,0 +1,13 @@
+package com.dk_power.power_plant_java.dto.categories;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SharedValueDto {
+    private Long id;
+    private Long objectType;
+}
