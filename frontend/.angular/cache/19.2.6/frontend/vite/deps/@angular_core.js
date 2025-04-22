21ca029e2586d4b29c2744c1534b8978927e869e
usadanilakl <usadanilakl@gmail.com>
2025-04-22 12:23:24 -0500

    changed style of form - row, column, reactive.

--- frontend/.angular/cache/19.2.6/frontend/vite/deps/@angular_core.js
+++ frontend/.angular/cache/19.2.6/frontend/vite/deps/@angular_core.js
@@ -495,9 +495,9 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-ZH43FRBS.js";
-import "./chunk-6Q4RANH6.js";
+} from "./chunk-HJS2BXAE.js";
 import "./chunk-FFZIAYYX.js";
+import "./chunk-6Q4RANH6.js";
 import "./chunk-CXCX2JKZ.js";
 export {
   ANIMATION_MODULE_TYPE,
