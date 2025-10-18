6e2d305c4fccfc0a9c53859d58fe59bee65af517
Danil <usadanilakl@gmail.com>
2025-10-18 14:22:49 -0500

    work request works, setting up jha

--- browser/ng-ui/src/app/features/jha/jha.component.ts
+++ browser/ng-ui/src/app/features/jha/jha.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { JhaFormComponent } from "./jha-form/jha-form.component";
+import { PopupComponent } from "../../shared/menus/popup/popup.component";
+import { JhaTableComponent } from "./jha-table/jha-table.component";
 
 @Component({
   selector: 'app-jha',
-  imports: [],
+  standalone: true,
+  imports: [JhaFormComponent, PopupComponent, JhaTableComponent],
   templateUrl: './jha.component.html',
   styleUrl: './jha.component.css'
 })
 export class JhaComponent {
+
+  isTablePopupOpen = false;
+  openTablePopup() {
+    this.isTablePopupOpen = true;
+  }
+  closeTablePopup() {
+    this.isTablePopupOpen = false;
+  }
 
 }
