8f8c0e81d99c1f29b5fdf1425a3d9983a4cd8932
Danil <usadanilakl@gmail.com>
2026-01-08 22:01:42 -0600

    Added Equipment Name category to be used for naming convension.

--- src/main/java/com/dk_power/power_plant_java/controller/angular/QandA/QandAController.java
+++ src/main/java/com/dk_power/power_plant_java/controller/angular/QandA/QandAController.java
@@ -1,8 +1,9 @@
-package com.dk_power.power_plant_java.controller.angular;
+package com.dk_power.power_plant_java.controller.angular.QandA;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/ng/question-and-answer")
