2010c5032335202c7a619ca5d214f091fef02259
Danil <usadanilakl@gmail.com>
2026-01-16 03:39:18 -0600

    Added tools to handle sync for large amounts of data and test service with UI for testing bulk sync.

---
+++ src/main/resources/static/angular/server/chunk-246VQUT3.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{ff as a}from"./chunk-CVHVFKGC.mjs";import"./chunk-S6KH3LOX.mjs";export{a as RfReactiveFormComponent};
--- src/main/resources/static/angular/server/chunk-6UQ45YMG.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{cf as a}from"./chunk-42DKSP32.mjs";import"./chunk-S6KH3LOX.mjs";export{a as RfReactiveFormComponent};
