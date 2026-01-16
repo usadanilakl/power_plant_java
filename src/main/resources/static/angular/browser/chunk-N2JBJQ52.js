8d565335bd4b17858eaefc6f61f5748aa02725d0
Danil <usadanilakl@gmail.com>
2026-01-16 03:41:00 -0600

    Added tools to handle sync for large amounts of data and test service with UI for testing bulk sync.

---
+++ src/main/resources/static/angular/browser/chunk-N2JBJQ52.js
@@ -0,0 +1,1 @@
+import{qe as a}from"./chunk-NU6TA33F.js";import"./chunk-EQDQRRRY.js";export{a as RfReactiveFormComponent};
--- src/main/resources/static/angular/browser/chunk-NSSEW4Q6.js
+++
@@ -1,1 +0,0 @@
-import{ne as a}from"./chunk-UC6LXHWC.js";import"./chunk-EQDQRRRY.js";export{a as RfReactiveFormComponent};
